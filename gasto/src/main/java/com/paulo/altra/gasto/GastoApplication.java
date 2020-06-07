70dcedbc9d5a2b7c116d113a70b9c3c8ac733ec1
Paulo Leite Costa <pauloleitecosta14@gmail.com>
2020-06-07 04:51:45 -0300

    add: Adicionando Servidor Recurso no Gasto e Categoria

--- gasto/src/main/java/com/paulo/altra/gasto/GastoApplication.java
+++ gasto/src/main/java/com/paulo/altra/gasto/GastoApplication.java
@@ -2,8 +2,10 @@ package com.paulo.altra.gasto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
+@EnableResourceServer
 public class GastoApplication {
 
 	public static void main(String[] args) {
